b3da63bcdf860bcb80af8507bea8dc1a3e9b8c14
JG <ludwing.gomez@epn.edu.ec>
2022-11-15 07:22:06 -0500

    Variables mutables e inmutables

---
+++ 01-JS/01-variables.js
@@ -0,0 +1,22 @@
+//mutables e inmutables
+//reasignar es cuando utilizo igual
+//mutables
+//var let
+//inmutables
+//val const
+//vamos a preferir CONST>LET>VAR!(CASI NUNCA)
+const numero=1;
+const sueldo=1.2;
+const texto='jair';
+const texto1="Gomez"
+const booleano=true;
+const hijos=null;
+const zapatos=undefined;
+
+console.log(typeof numero);
+console.log(typeof sueldo)
+console.log(typeof texto)
+console.log(typeof texto1)
+console.log(typeof booleano)
+console.log(typeof hijos)
+console.log(typeof  zapatos)
