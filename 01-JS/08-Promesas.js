5861430d7e30c94dd868257fd9aba9f4cf52198b
JG <ludwing.gomez@epn.edu.ec>
2023-01-08 21:44:14 -0500

    ASYNC AWAIT

--- 01-JS/08-Promesas.js
+++ 01-JS/08-Promesas.js
@@ -56,7 +56,7 @@ ejercicio08("06-ejemplo.txt", " :) lo logramos")
     .then()
     .catch()
 
-
+/*
 function ejercicio(path,contenidoArchivo){
     return new Promise(
         (res,rej)=>{
@@ -83,3 +83,34 @@ ejercicio("06-ejemplo.txt", " :) lo logramos")
             return escribirArchivo("06-ejemplo.txt", algo+ "uwu")
         })
     .catch()
+ */
+
+// ASYNC AWAIT
+// REGLAS:
+// 1) Estar dentro de una funcion (nombrada o anonima)
+// 2) AGREGAR la palabra 'async' antes de la declaracion
+//    de la funcion
+// 3) AGREGAR la palabra 'await' antes de la declaracion
+//    de una promesa
+async function asyncAwaitUno(path, nuevoContenido) {
+    // Si sabemos que en la promesa PUEDE haber un reject, usamos try y catch
+    try {
+        //AL SER ASÍNCRONO PUEDE QUE SIGA AVANZANDO LAS LINEAS DE CÓDIGO
+        // PERO AL PONER AWAIT ES COMO QUE CORTAMOS ESE FLUJO, Y NO AVANZA HASTA QUE TERMINE ESA FUNCIÓN
+        const respuestaContenidoArchivoOriginal = await leerArchivo(path);
+        await escribirArchivo(path, respuestaContenidoArchivoOriginal + nuevoContenido );
+        // await escribirArchivo(path, (await leerArchivo(path)) + nuevoContenido );
+        1 + 1; // caramelo
+        leerArchivo().then().catch() // async
+        await leerArchivo() // sync
+
+        //ASYN Y AWAIT es la forma en que se eliminó el .then, y .catch (promesas), las promesas arreglan los callbacks
+    } catch (error) {
+        console.error(error);
+    }
+}
+asyncAwaitUno('06-ejemplo.txt', ' :)  lo logramos!').then().catch()
+const asyncAwaitDos = async function () {
+}
+const asyncAwaitTres = async () => {
+}
