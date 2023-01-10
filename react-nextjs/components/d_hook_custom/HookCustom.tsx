ed626f8f7efc8d910b08e6005543f3be574ad50a
JG <ludwing.gomez@epn.edu.ec>
2023-01-10 10:58:14 -0500

    componente incompleto
    Error en el map

---
+++ react-nextjs/components/d_hook_custom/HookCustom.tsx
@@ -0,0 +1,10 @@
+import HookCustom from "../d_hook_custom/H"
+
+export default function (){
+    return (
+        <>
+            <h1>Hook Custom</h1>
+            <HookCustom></HookCustom>
+        </>
+    )
+}
